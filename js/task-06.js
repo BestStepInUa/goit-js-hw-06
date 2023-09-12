d846b7e465ec5f85ad9ef8076c0a6eb657d688c5
BestStepInUa <beststep.in.ua@gmail.com>
2023-09-12 14:35:30 +0300

    task_6_fix

--- js/task-06.js
+++ js/task-06.js
@@ -29,12 +29,6 @@
 const inputRef = document.getElementById('validation-input')
 const inputDataLength = Number(inputRef.dataset.length);
 
-// console.log(typeof inputDataLength);
-// console.log(inputDataLength);
-// console.log(typeof inputValueLength);
-// console.log(inputValueLength);
-
-
 inputRef.addEventListener('blur', onInputBlur);
 inputRef.addEventListener('focus', onInputFocus);
 
@@ -47,12 +41,12 @@ function onInputBlur() {
   // console.log(inputValueLength === inputDataLength);
 
   if (inputValueLength === inputDataLength) {
-    inputRef.classList.add('.valid');
+    inputRef.classList.add('valid');
   } else {
-    inputRef.classList.add('.invalid');
+    inputRef.classList.add('invalid');
   }
 };
 
 function onInputFocus() {
-  inputRef.classList.remove('.valid', '.invalid');
+  inputRef.classList.remove('valid', 'invalid');
 };
