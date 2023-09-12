7282cc9bc2ef1476fb649cdd1bc454fac5741c3c
BestStepInUa <beststep.in.ua@gmail.com>
2023-09-12 14:26:25 +0300

    task_6_need_mentor_help

--- js/task-06.js
+++ js/task-06.js
@@ -26,12 +26,33 @@
   border-color: #f44336;
 } */
 
-const inputRef = document.getElementById('validation-input');
+const inputRef = document.getElementById('validation-input')
+const inputDataLength = Number(inputRef.dataset.length);
+
+// console.log(typeof inputDataLength);
+// console.log(inputDataLength);
+// console.log(typeof inputValueLength);
+// console.log(inputValueLength);
+
 
 inputRef.addEventListener('blur', onInputBlur);
+inputRef.addEventListener('focus', onInputFocus);
 
 function onInputBlur() {
-  if (inputRef.textContent.length !== inputRef.data-length) {
-    inputRef.classList.add('.invalid')
+  const inputValueLength = inputRef.value.length;
+  // console.log(typeof inputDataLength);
+  // console.log(inputDataLength);
+  // console.log(typeof inputValueLength);
+  // console.log(inputValueLength);
+  // console.log(inputValueLength === inputDataLength);
+
+  if (inputValueLength === inputDataLength) {
+    inputRef.classList.add('.valid');
+  } else {
+    inputRef.classList.add('.invalid');
   }
-}
+};
+
+function onInputFocus() {
+  inputRef.classList.remove('.valid', '.invalid');
+};
